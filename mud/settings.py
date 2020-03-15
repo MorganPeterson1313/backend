956f00119f5955a35368ef9209523ec7e7b49b08
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-03-14 23:41:46 -0400

    cleaned up code

--- mud/settings.py
+++ mud/settings.py
@@ -160,11 +160,4 @@ STATIC_URL = '/static/'
 import django_heroku
 django_heroku.settings(locals())
 
-# from corsheaders.signals import check_request_enabled
-
-# def cors_allow_api_to_everyone(sender, request, **kwargs):
-#     return request.path.startswith('/api/')
-
-# check_request_enabled.connect(cors_allow_api_to_everyone)
-
 
