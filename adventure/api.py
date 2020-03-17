33ec1db296a8a3132e0fd7bcc5bcc91730e31457
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-03-16 21:39:58 -0400

    changed api.py initialize view to POST

--- adventure/api.py
+++ adventure/api.py
@@ -12,7 +12,7 @@ import json
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
 @csrf_exempt
-@api_view(["GET"])
+@api_view(["POST"])
 def initialize(request):
     user = request.user
     player = user.player
